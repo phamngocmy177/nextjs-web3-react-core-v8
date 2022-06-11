674a7d1bba217b537ebac336d5b3c12cfc612245
ngocmy <phamngocmy177@gmail.com>
2022-06-12 00:41:32 +0700

    feat: inital project

---
+++ components/ProviderExample.tsx
@@ -0,0 +1,39 @@
+import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
+import { Web3ReactHooks } from '@web3-react/core'
+import { MetaMask } from '@web3-react/metamask'
+import { Network } from '@web3-react/network'
+import type { Connector } from '@web3-react/types'
+import { WalletConnect } from '@web3-react/walletconnect'
+import { coinbaseWallet, hooks as coinbaseWalletHooks } from '../connectors/coinbaseWallet'
+import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask'
+import { hooks as networkHooks, network } from '../connectors/network'
+import { hooks as walletConnectHooks, walletConnect } from '../connectors/walletConnect'
+
+function getName(connector: Connector) {
+  if (connector instanceof MetaMask) return 'MetaMask'
+  if (connector instanceof WalletConnect) return 'WalletConnect'
+  if (connector instanceof CoinbaseWallet) return 'Coinbase Wallet'
+  if (connector instanceof Network) return 'Network'
+  return 'Unknown'
+}
+
+const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network, Web3ReactHooks][] = [
+  [metaMask, metaMaskHooks],
+  [walletConnect, walletConnectHooks],
+  [coinbaseWallet, coinbaseWalletHooks],
+  [network, networkHooks],
+]
+
+function Child() {
+    const provider = metaMaskHooks.useProvider()
+
+    const { library } = metaMaskHooks.useWeb3React(provider)
+
+  return <span>Provider</span>
+}
+
+export default function ProviderExample() {
+  return (
+      <Child />
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    infuraKey: process.env.INFURA_KEY,
+    alchemyKey: process.env.ALCHEMY_KEY,
+    magicKey: process.env.MAGIC_KEY,
+  },
 }
 
 module.exports = nextConfig
